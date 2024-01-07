a7474dffe286bc08ce6d03f3b2d7f3b5d9cb564c
Mark Dolgov <dolgov.me@gmail.com>
2024-01-07 15:03:31 +0300

    Add BookSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from api.models import Book
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "first_name", "last_name", "email"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "release_date"]
