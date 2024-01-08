780ee97d61c46daa5dcfc672a0f662dd38180967
Mark Dolgov <dolgov.me@gmail.com>
2024-01-09 00:27:58 +0300

    Create Views

--- api/views.py
+++ api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 
-from api.serializers import UserSerializer
+from api.models import Order, Book, Shop, OrderItem, Cart
+from api.serializers import UserSerializer, OrderSerializer, BookSerializer, ShopSerializer, OrderItemSerializer, \
+    CartSerializer
 
 
 class UserListView(generics.ListAPIView):
@@ -13,3 +15,53 @@ class UserListView(generics.ListAPIView):
 class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class ShopListView(generics.ListAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+
+class ShopDetailView(generics.RetrieveAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+
+class CartListView(generics.ListCreateAPIView, generics.DestroyAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+    # def delete(self, request, *args, **kwargs): todo continue!!!
+
+
+class OrderListView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemListView(generics.ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class OrderItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
