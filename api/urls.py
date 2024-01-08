b06dfbaffe4bc9e5a572a69f0ca302f0a099e47d
Mark Dolgov <dolgov.me@gmail.com>
2024-01-09 00:30:51 +0300

    Add urls

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('users', views.UserListView.as_view()),
     path('users/<int:pk>', views.UserDetailView.as_view()),
+    path('books', views.BookListView.as_view()),
+    path('books/<int:pk>', views.BookDetailView.as_view()),
+    path('shops', views.ShopListView.as_view()),
+    path('shops/<int:pk>', views.ShopDetailView.as_view()),
+    path('cart', views.CartListView.as_view()),
+    path('orders', views.OrderListView.as_view()),
+    path('orders/<int:pk>', views.OrderDetailView.as_view()),
+    path('order-items', views.OrderItemListView.as_view()),
+    path('order-items/<int:pk>', views.OrderItemDetailView.as_view()),
 ]
